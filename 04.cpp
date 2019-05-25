92af3cfbeb3e7d14f01b77193524e9c3e9d790ef
safoorazangeneh <50417205+safoorazangeneh@users.noreply.github.com>
2019-05-25 18:36:06 +0430

    Add files via upload

---
+++ 04.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+   main(){
+	int number;
+	cin>>number;
+	cout<<"enter number";
+	if(number%2==0){
+
+		cout<<":"<<"zoje";}
+		else{
+			cout<<":"<<"fard";
+		}
+	} 
