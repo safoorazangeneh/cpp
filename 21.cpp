17e3e11cce0cce7eb3e0a0461e5815f32e745926
safoorazangeneh <50417205+safoorazangeneh@users.noreply.github.com>
2019-05-29 16:09:01 +0430

    Add files via upload

---
+++ 21.cpp
@@ -0,0 +1,27 @@
+#include<iostream.h>
+main(){
+	int a;
+	cin>>a;
+	cout<<please enter rozaye hafteh;
+	switch(a){
+		case1:
+			cout<<"shanbeh";
+			break;
+			case2:
+				cout<<"1shanbeh";
+				break;
+				case3:
+					cout<<"2shanbeh";
+					break;
+						case4:
+							cout<<"3shanbeh";
+							break;
+							case5:
+								cout<<"4shanbeh";
+								break;
+								case6:
+									cout<<"5shanbeh";
+									break;
+									deafult:
+										cout<<"jome";
+	}
