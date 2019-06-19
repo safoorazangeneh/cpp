39db041c1a0ced44fbce40c0d607959026413fc2
safoorazangeneh <50417205+safoorazangeneh@users.noreply.github.com>
2019-06-19 17:58:51 +0430

    Add files via upload

---
+++ 01area p,s.cpp
@@ -0,0 +1,14 @@
+#include<conio.h>
+using namespace std;
+double area(double r, p){
+	cout<<"enter a area";
+  return r*r*3.14;
+  return r*z*3.14;
+	}
+	main(){
+	double r,p,result;
+	cin>>r>>p;
+		result=area(r,p);
+		cout<<result;
+	}
+
