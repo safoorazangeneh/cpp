094739a93b1e5f9087253c943013839c28193eed
safoorazangeneh <50417205+safoorazangeneh@users.noreply.github.com>
2019-06-19 18:28:45 +0430

    Add files via upload

---
+++ 02a,b,c.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main(){
+	int a,b,c;
+	int n;
+	cout<<"please enter addad:"<<endl;
+	cin>>n;
+	for(a=1;a<=n;a++)
+	for(b=1;a<=b;b++)
+	for(c=1;b<=c;c++)
+	cout<<c<<'\t';
+	cout<<endl;
+	getch();
+}
