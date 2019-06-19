f63b8b5c19d3dcbf9d621e4250af361717b6dbdb
safoorazangeneh <50417205+safoorazangeneh@users.noreply.github.com>
2019-06-19 18:35:39 +0430

    Add files via upload

---
+++ 03.2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+bool oddeven( int n){
+	if(n%2==0)
+		return true;
+	return false;}
+	main(){
+		int n;
+		cin>>n;
+		if(oddeven(n)==true)
+		cout<<"even";
+		else
+			cout<<"odd";
+	
+		
+		
+		
+	  
+	
+}
